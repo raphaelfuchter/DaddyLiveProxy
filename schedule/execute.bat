520a4206b7aa5c5a084697980edb9898e0530659
Raphael Fuchter <raphael.fuchter@gmail.com>
2025-07-21 15:56:39 -0300

    corrigido bat

--- schedule/execute.bat
+++ schedule/execute.bat
@@ -1,6 +1,8 @@
 @echo off
 echo --- Executando o script para gerar a playlist e o EPG...
+
 python schedule_down.py
+
 echo.
-echo --- Iniciando o servidor local...
-python iniciar_servidor.py
+echo --- Script finalizado. ---
+pause
