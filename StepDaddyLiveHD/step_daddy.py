a2575d048bdce702f4c51a920dda86ec5dc22111
Raphael Fuchter <raphael.fuchter@gmail.com>
2025-09-01 09:32:54 -0300

    Implementado cache da url base

--- StepDaddyLiveHD/step_daddy.py
+++ StepDaddyLiveHD/step_daddy.py
@@ -35,6 +35,7 @@ class StepDaddy:
         self.channels = []
         with open("StepDaddyLiveHD/meta.json", "r") as f:
             self._meta = json.load(f)
+        self._cache = {}
 
     def _headers(self, referer: str = None, origin: str = None):
         settings = self._load_settings()
@@ -58,7 +59,7 @@ class StepDaddy:
         try:
             response = await self._session.get(f"{base_url}/24-7-channels.php", headers=self._headers())
             channels_block = re.compile("<center><h1(.+?)tab-2", re.MULTILINE | re.DOTALL).findall(str(response.text))
-            channels_data = re.compile("href=\"(.*)\" target(.*)<strong>(.*)</strong>").findall(channels_block[0])
+            channels_data = re.compile('''href=\\"(.*)\\" target(.*)<strong>(.*)</strong>''').findall(channels_block[0])
             for channel_data in channels_data:
                 channels.append(self._get_channel(channel_data))
         finally:
@@ -103,100 +104,96 @@ class StepDaddy:
     async def stream(self, channel_id: str):
         logging.info(f"Iniciando método stream para o channel_id: {channel_id}")
 
-        settings = self._load_settings()
-        base_url = settings["base_url"]
-        user_prefix = settings["prefix"]
-        logging.info(f"Configurações carregadas: base_url='{base_url}', prefix='{user_prefix}'")
+        if self._cache.get("channel_id") == channel_id:
+            logging.info(f"Usando server_url e source_url do cache para o canal {channel_id}")
+            server_url = self._cache["server_url"]
+            source_url = self._cache["source_url"]
+        else:
+            self._cache.clear()
+            settings = self._load_settings()
+            base_url = settings["base_url"]
+            user_prefix = settings["prefix"]
+            logging.info(f"Configurações carregadas: base_url='{base_url}', prefix='{user_prefix}'")
 
-        key = "CHANNEL_KEY"
+            key = "CHANNEL_KEY"
+            prefixes = [user_prefix]
+            source_url = None
+            source_response = None
 
-        # ALTERADO: A lista de prefixes agora usa o valor carregado do settings.json
-        prefixes = [user_prefix]
+            for prefix in prefixes:
+                logging.debug(f"Tentando com o prefixo: '{prefix}'")
+                url = f"{base_url}/{prefix}/stream-{channel_id}.php"
+                if len(channel_id) > 3:
+                    url = f"{base_url}/stream/bet.php?id=bet{channel_id}"
 
-        source_url = None
-        source_response = None
+                logging.debug(f"Construindo URL de requisição: {url}")
+                response = await self._session.post(url, headers=self._headers())
+                logging.info(f"Resposta recebida de {url} com status: {response.status_code}")
 
-        for prefix in prefixes:
-            logging.debug(f"Tentando com o prefixo: '{prefix}'")
-
-            # ALTERADO: A URL agora é construída com a `base_url` do settings.json
-            url = f"{base_url}/{prefix}/stream-{channel_id}.php"
-            if len(channel_id) > 3:
-                # ALTERADO: A URL agora é construída com a `base_url` do settings.json
-                url = f"{base_url}/stream/bet.php?id=bet{channel_id}"
-
-            logging.debug(f"Construindo URL de requisição: {url}")
-            response = await self._session.post(url, headers=self._headers())
-            logging.info(f"Resposta recebida de {url} com status: {response.status_code}")
-
-            matches = re.compile("iframe src=\"(.*)\" width").findall(response.text)
-            if matches:
-                source_url = matches[0]
-                logging.debug(f"Iframe encontrado com source_url: {source_url}")
-
-                source_response = await self._session.post(source_url, headers=self._headers(url))
-                logging.info(f"Resposta recebida do source_url com status: {source_response.status_code}")
-
-                if key in source_response.text:
-                    logging.debug(f"'{key}' encontrada na resposta. Saindo do loop de prefixos.")
-                    break
+                matches = re.compile('''iframe src=\\"(.*)\\" width''').findall(response.text)
+                if matches:
+                    source_url = matches[0]
+                    logging.debug(f"Iframe encontrado com source_url: {source_url}")
+                    source_response = await self._session.post(source_url, headers=self._headers(url))
+                    logging.info(f"Resposta recebida do source_url com status: {source_response.status_code}")
+                    if key in source_response.text:
+                        logging.debug(f"'{key}' encontrada na resposta. Saindo do loop de prefixos.")
+                        break
+                    else:
+                        logging.debug(f"'{key}' não encontrada na resposta do source_url.")
                 else:
-                    logging.debug(f"'{key}' não encontrada na resposta do source_url.")
+                    logging.debug("Nenhum iframe encontrado na resposta.")
             else:
-                logging.debug("Nenhum iframe encontrado na resposta.")
-        else:
-            logging.error("Falha ao encontrar uma source_url válida para o canal.")
-            print("")
-            print("")
-            print("")
-            raise ValueError("Failed to find source URL for channel")
+                logging.error("Falha ao encontrar uma source_url válida para o canal.")
+                raise ValueError("Failed to find source URL for channel")
 
-        channel_key = re.compile(rf"const\s+{re.escape(key)}\s*=\s*\"(.*?)\";").findall(source_response.text)[-1]
-        logging.debug(f"Channel key extraída: {channel_key}")
+            channel_key = re.compile(rf'''const\s+{re.escape(key)}\s*=\s*\"(.*?)\";''').findall(source_response.text)[-1]
+            logging.debug(f"Channel key extraída: {channel_key}")
 
-        bundle = re.compile(r"const\s+XJZ\s*=\s*\"(.*?)\";").findall(source_response.text)[-1]
-        logging.debug(f"Bundle encontrado: {bundle[:30]}...")  # Loga apenas o início do bundle
+            bundle = re.compile(r'''const\s+XJZ\s*=\s*\"(.*?)\";''').findall(source_response.text)[-1]
+            logging.debug(f"Bundle encontrado: {bundle[:30]}...")  # Loga apenas o início do bundle
 
-        data = decode_bundle(bundle)
-        auth_ts = data.get("b_ts", "")
-        auth_sig = data.get("b_sig", "")
-        auth_rnd = data.get("b_rnd", "")
-        auth_url = data.get("b_host", "")
-        logging.debug(
-            f"Dados do bundle decodificados: auth_ts='{auth_ts}', auth_sig='{auth_sig}', auth_rnd='{auth_rnd}', auth_url='{auth_url}'")
+            data = decode_bundle(bundle)
+            auth_ts = data.get("b_ts", "")
+            auth_sig = data.get("b_sig", "")
+            auth_rnd = data.get("b_rnd", "")
+            auth_url = data.get("b_host", "")
+            logging.debug(
+                f"Dados do bundle decodificados: auth_ts='{auth_ts}', auth_sig='{auth_sig}', auth_rnd='{auth_rnd}', auth_url='{auth_url}'")
 
-        auth_request_url = f"{auth_url}auth.php?channel_id={channel_key}&ts={auth_ts}&rnd={auth_rnd}&sig={auth_sig}"
-        logging.debug(f"URL de autenticação: {auth_request_url}")
+            auth_request_url = f"{auth_url}auth.php?channel_id={channel_key}&ts={auth_ts}&rnd={auth_rnd}&sig={auth_sig}"
+            logging.debug(f"URL de autenticação: {auth_request_url}")
 
-        auth_response = await self._session.get(auth_request_url, headers=self._headers(source_url))
-        logging.debug(f"Resposta da autenticação recebida com status: {auth_response.status_code}")
+            auth_response = await self._session.get(auth_request_url, headers=self._headers(source_url))
+            logging.debug(f"Resposta da autenticação recebida com status: {auth_response.status_code}")
 
-        if auth_response.status_code != 200:
-            logging.error("Falha ao obter resposta da autenticação.")
-            print("")
-            print("")
-            print("")
-            raise ValueError("Failed to get auth response")
+            if auth_response.status_code != 200:
+                logging.error("Falha ao obter resposta da autenticação.")
+                raise ValueError("Failed to get auth response")
 
-        key_url = urlparse(source_url)
-        key_url = f"{key_url.scheme}://{key_url.netloc}/server_lookup.php?channel_id={channel_key}"
-        logging.debug(f"URL para lookup do servidor: {key_url}")
+            key_url = urlparse(source_url)
+            key_url = f"{key_url.scheme}://{key_url.netloc}/server_lookup.php?channel_id={channel_key}"
+            logging.debug(f"URL para lookup do servidor: {key_url}")
 
-        key_response = await self._session.get(key_url, headers=self._headers(source_url))
-        logging.debug(f"Resposta do lookup do servidor recebida com status: {key_response.status_code}")
+            key_response = await self._session.get(key_url, headers=self._headers(source_url))
+            logging.debug(f"Resposta do lookup do servidor recebida com status: {key_response.status_code}")
 
-        server_key = key_response.json().get("server_key")
-        logging.debug(f"Server key obtida: {server_key}")
+            server_key = key_response.json().get("server_key")
+            logging.debug(f"Server key obtida: {server_key}")
 
-        if not server_key:
-            logging.error("Nenhuma server_key encontrada na resposta.")
-            raise ValueError("No server key found in response")
+            if not server_key:
+                logging.error("Nenhuma server_key encontrada na resposta.")
+                raise ValueError("No server key found in response")
 
-        if server_key == "top1/cdn":
-            server_url = f"https://top1.newkso.ru/top1/cdn/{channel_key}/mono.m3u8"
-        else:
-            server_url = f"https://{server_key}new.newkso.ru/{server_key}/{channel_key}/mono.m3u8"
-        logging.debug(f"URL do m3u8 final: {server_url}")
+            if server_key == "top1/cdn":
+                server_url = f"https://top1.newkso.ru/top1/cdn/{channel_key}/mono.m3u8"
+            else:
+                server_url = f"https://{server_key}new.newkso.ru/{server_key}/{channel_key}/mono.m3u8"
+            logging.debug(f"URL do m3u8 final: {server_url}")
+
+            self._cache["channel_id"] = channel_id
+            self._cache["server_url"] = server_url
+            self._cache["source_url"] = source_url
 
         m3u8 = await self._session.get(server_url, headers=self._headers(quote(str(source_url))))
         logging.debug(f"m3u8 obtido com status: {m3u8.status_code}")
@@ -204,7 +201,7 @@ class StepDaddy:
         m3u8_data = ""
         for line in m3u8.text.split("\n"):
             if line.startswith("#EXT-X-KEY:"):
-                original_url = re.search(r'URI="(.*?)"', line).group(1)
+                original_url = re.search(r'''URI=\"(.*?)\"''', line).group(1)
                 new_url = f"{config.api_url}/key/{encrypt(original_url)}/{encrypt(urlparse(source_url).netloc)}"
                 line = line.replace(original_url, new_url)
                 logging.debug(f"Linha de chave #EXT-X-KEY modificada para: {line}")
@@ -217,11 +214,6 @@ class StepDaddy:
             m3u8_data += line + "\n"
 
         logging.info("Processamento do m3u8 finalizado. Retornando dados.")
-        print(m3u8_data)
-
-        print("")
-        print("")
-        print("")
         return m3u8_data
 
     async def key(self, url: str, host: str):
@@ -239,7 +231,7 @@ class StepDaddy:
     def playlist(self):
         data = "#EXTM3U\n"
         for channel in self.channels:
-            entry = f" tvg-logo=\"{channel.logo}\",{channel.name}" if channel.logo else f",{channel.name}"
+            entry = f''' tvg-logo=\"{channel.logo}\",{channel.name}''' if channel.logo else f",{channel.name}"
             data += f"#EXTINF:-1{entry}\n{config.api_url}/stream/{channel.id}.m3u8\n"
         return data
 
